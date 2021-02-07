ac0bcd3e410c3e900695ca5c8fd79735037429e3
unknown <timgels1808@hotmail.com>
2021-02-07 22:09:53 +0100

    You can now input your own input and output directory paths

--- SubtitleExtraction.ps1
+++ SubtitleExtraction.ps1
@@ -1,9 +1,14 @@
+param (
+    [string]$inputDir = "Input\",
+    [string]$outputDir = "Output\"
+)
+
 Write-Host "Extracting subtitles from files."
 
-$files = Get-ChildItem -Path "Input\" | Where-Object { $_.PSIsContainer -eq $false -and $_.Extension -eq ".mkv" }
+$files = Get-ChildItem -Path "$inputDir" | Where-Object { $_.PSIsContainer -eq $false -and $_.Extension -eq ".mkv" }
 
 foreach ($file in $files) {
     $subtitleFileName = $file.BaseName + '.ssa'
-    ffmpeg.exe -i "Input\$file" -map 0:s:1 "Output\$subtitleFileName"
-    Write-Host $subtitleFileName
+    Write-Host $file
+    ffmpeg.exe -i "$inputDir\$file" -map 0:s:1 "$outputDir\$subtitleFileName"
 }
