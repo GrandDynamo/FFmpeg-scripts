408b82406971cbaafa8c841974919943373ed990
unknown <timgels1808@hotmail.com>
2021-02-07 21:39:20 +0100

    fixed wrong in and output ffmpeg

--- SubtitleExtraction.ps1
+++ SubtitleExtraction.ps1
@@ -1,9 +1,9 @@
 Write-Host "Extracting subtitles from files."
 
-$files = Get-ChildItem -Path "Videos\" | Where-Object { $_.PSIsContainer -eq $false -and $_.Extension -eq ".mkv" }
+$files = Get-ChildItem -Path "Input\" | Where-Object { $_.PSIsContainer -eq $false -and $_.Extension -eq ".mkv" }
 
 foreach ($file in $files) {
     $subtitleFileName = $file.BaseName + '.ssa'
-    # ffmpeg.exe -i "Videos\$file" -map 0:s:1 "Output\$subtitleFileName"
+    ffmpeg.exe -i "Input\$file" -map 0:s:1 "Output\$subtitleFileName"
     Write-Host $subtitleFileName
 }
